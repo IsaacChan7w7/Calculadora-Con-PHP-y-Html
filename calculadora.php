6fee6e089e3c05bd2d6bd5e3a75b24b404cdc8eb
Isaac Orlando Cinco Leon <42479678+IsaacChan7w7@users.noreply.github.com>
2018-08-19 13:43:58 -0700

    Add files via upload

---
+++ calculadora.php
@@ -0,0 +1,29 @@
+<!--
+codigo creado por isaac orlando cinco leon
+-->
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Calculadora Por Isaac Orlando Cinco Leon</title>
+</head>
+<body>
+<h1>
+	<center>Calculadora Por Isaac Orlando Cinco Leon</center>
+</h1>
+<h2>
+   <center>Esta Calculadora Fue Hecha Por El Usuario De Github : <a href="https://github.com/IsaacChan7w7">IsaacChan7w7</a></center>
+</h2>
+<hr size="5px">
+<br>
+<center>
+<form action="http://localhost/calculadora/resultado.php" method="post">
+<input type="text" name="numero1" placeholder="Pon Un Numero">
+<p>+</p>
+<input type="text" name="numero2" placeholder="Pon El Segundo Numero">
+<br>
+<br>
+<button type="text" name="sumar">Sumar</button>
+</form>
+</center>
+</body>
+</html>
---
+++ resultado.php
@@ -0,0 +1,39 @@
+<!--
+codigo creado por isaac orlando cinco leon
+-->
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Calculadora Por Isaac Orlando Cinco Leon</title>
+</head>
+<body>
+<h1>
+	<center>Calculadora Por Isaac Orlando Cinco Leon</center>
+</h1>
+<h2>
+   <center>Esta Calculadora Fue Hecha Por El Usuario De Github : <a href="https://github.com/IsaacChan7w7">IsaacChan7w7</a></center>
+</h2>
+<hr size="5px">
+<br>
+<?php
+if($_POST)
+	{	
+		$numero1 = $_POST
+		['numero1'];
+		$numero2 = $_POST
+		['numero2'];
+		$suma = $numero1 + $numero2; 
+	}
+echo "<h2><center>El Resultado De La Suma Total Es : $suma</center></h2>";
+?>
+<hr size="5px">
+<br>
+<br>
+<h3>
+	<center>Donaciones Para Seguir Creciendo En La Programacion :3</center>
+<br>
+	<center>Bitcoin : 3QSWCVxBRTMpTpq5d9ta2dyrRtUEgoWcB2</center>
+	<center>Dogecoin : DLVUPTv7oUKfWY9ewUEqUJE6G3NNBmYi4s </center>
+</h3>
+</body>
+</html>
